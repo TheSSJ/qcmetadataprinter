2d2dadd3634bf4f82908eabdcceae95d09d47afd
TheSSJ <greg.house.ssj@gmail.com>
2020-05-19 11:00:25 +0000

    tulip: write feedback to file instead of console
    To better work with the output, it is better to just pull a file from /data/tmp
    instead of scanning console output or logcat (tried both...)

--- get_offsets.c
+++ get_offsets.c
@@ -12,9 +12,9 @@
     oss_pointer = (char*)(POINTER_OF_META(PARAM_ID, table_ptr)) - (char*)(table_ptr);               \
     blob_size = get_size_of(PARAM_ID);                                                              \
     oss_size = SIZE_OF_PARAM(PARAM_ID, table_ptr);                                                  \
-    if ((blob_pointer != oss_pointer) || (blob_size != oss_size)) printf("FIXME!!!\n");             \
-    printf("BLOB: %s index=%d pointer=%d size=%d\n", #PARAM_ID, PARAM_ID, blob_pointer, blob_size); \
-    printf("OSS : %s index=%d pointer=%d size=%d\n", #PARAM_ID, PARAM_ID, oss_pointer, oss_size);
+    if ((blob_pointer != oss_pointer) || (blob_size != oss_size)) fprintf(fil,"FIXME!!!\n");             \
+    fprintf(fil, "BLOB: %s index=%d pointer=%d size=%d\n", #PARAM_ID, PARAM_ID, blob_pointer, blob_size); \
+    fprintf(fil, "OSS : %s index=%d pointer=%d size=%d\n", #PARAM_ID, PARAM_ID, oss_pointer, oss_size);
 
 typedef void* (*get_pointer_of_t)(cam_intf_parm_type_t, metadata_buffer_t*);
 typedef uint32_t (*get_size_of_t)(cam_intf_parm_type_t);
@@ -39,22 +39,24 @@ int main() {
     get_pointer_of_t get_pointer_of;
     get_size_of_t get_size_of;
     metadata_buffer_t* pMetadata = malloc(sizeof(metadata_buffer_t));
+    FILE *fil;
+    fil = fopen("/data/tmp/get_offset.log", "w");
 
     handle = find_library_handle();
     if (!handle) {
-        fprintf(stderr, "%s\n", dlerror());
+        fprintf(fil, "%s\n", dlerror());
         return 1;
     }
 
     get_pointer_of = (get_pointer_of_t)dlsym(handle, "get_pointer_of");
     if (!get_pointer_of) {
-        fprintf(stderr, "%s\n", dlerror());
+        fprintf(fil, "%s\n", dlerror());
         return 1;
     }
 
     get_size_of = (get_size_of_t)dlsym(handle, "get_size_of");
     if (!get_size_of) {
-        fprintf(stderr, "%s\n", dlerror());
+        fprintf(fil, "%s\n", dlerror());
         return 1;
     }
 
@@ -66,5 +68,6 @@ int main() {
     /* INCLUDE() macros from metadata_data_t go after this */
 
     dlclose(handle);
+    fclose(fil);
     return 0;
 }
